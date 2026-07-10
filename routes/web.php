bd6502e1c606e4f523ebbdfb14180918e4a8f7fb
emit <emitjose17@gmail.com>
2026-07-10 16:27:29 +0530

    Ecommer project committed

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,3 @@ use App\Http\Controllers\Admin\ProductImportController;
 Route::get('/import', [ProductImportController::class, 'index'])->name('products.import');
 Route::post('/import', [ProductImportController::class, 'import'])->name('products.import.post');
 
-Route::get('/')
