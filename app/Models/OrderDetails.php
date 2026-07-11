3a9f5591b6be5ccbbccce1ed68de324caf191d0f
emit <emitjose17@gmail.com>
2026-07-11 11:59:46 +0530

    Order and Order details Model

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetails extends Model
 {
     //
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'price',
+        'quantity',
+        'total',
+    ];
+
+    /**
+     * Order detail belongs to an order.
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * Order detail belongs to a product.
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
-    //
+     protected $fillable = [
+        'user_id',
+        'order_number',
+        'payment_method',
+        'payment_id',
+        'order_status',
+    ];
+
+    /**
+     * Order belongs to a user.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Order has many order details.
+     */
+    public function details()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+    
 }
