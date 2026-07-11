5a9903084b4ad346fb30686f9e3b07ad961eb784
emit <emitjose17@gmail.com>
2026-07-11 13:33:46 +0530

    Order Cancel applied on order service

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -14,33 +14,35 @@ class OrderService
 
         try{
 
-            DB::beginTransaction();
+            $order = DB::transaction(function () use ($data) {
 
-            $order = Order::create([
-                'user_id'=>$data['user_id'],
-                'order_number'=>Str::random(10),
-                'payment_method'=>$data['payment_method'],
-                'payment_id'=>$data['payment_id'],
-                'order_status'=>$data['order_status'],
-            ]);
-
-            foreach ($data['products'] as $item) {
-
-                $product = Product::findOrFail($item['product_id']);
-
-                OrderDetail::create([
-                    'order_id'   => $order->id,
-                    'product_id' => $product->id,
-                    'price'      => $product->price,
-                    'quantity'   => $item['quantity'],
-                    'total'      => $product->price * $item['quantity'],
+                $order = Order::create([
+                    'user_id'        => $data['user_id'],
+                    'order_number'   => Str::random(10),
+                    'payment_method' => $data['payment_method'],
+                    'payment_id'     => $data['payment_id'],
+                    'order_status'   => $data['order_status'],
                 ]);
 
-                // Reduce Stock
-                //$product->decrement('stock', $item['quantity']);
-            }
+                foreach ($data['products'] as $item) {
 
-            DB::commit();
+                    $product = Product::findOrFail($item['product_id']);
+
+                    OrderDetail::create([
+                        'order_id'   => $order->id,
+                        'product_id' => $product->id,
+                        'price'      => $product->price,
+                        'quantity'   => $item['quantity'],
+                        'total'      => $product->price * $item['quantity'],
+                    ]);
+
+                    // Reduce Stock
+                    // $product->decrement('stock', $item['quantity']);
+                }
+
+                return $order;
+            });
+
             return $order;
 
         }catch(Exception $e){
@@ -50,4 +52,33 @@ class OrderService
 
     }
 
+    public function cancel(Order $order){
+
+        try{
+
+            $order = DB::transaction(function () use ($order) {
+
+                $order->update([
+                    'order_status'   => 'cancelled',
+                ]);
+
+                foreach ($order->details as $item) {
+
+                    $product = Product::findOrFail($item['product_id']);
+
+                    $product->increment('stock', $item['quantity']);
+                }
+
+                return $order;
+            });
+
+            return $order;
+
+        }catch(Exception $e){
+            DB::rollBack();
+            throw $e;
+        }
+
+    }
+ 
 }
