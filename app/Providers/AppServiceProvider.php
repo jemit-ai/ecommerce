5c97958e7d84fd69f980137f396b9880bcaf4a13
emit <emitjose17@gmail.com>
2026-07-14 10:48:49 +0530

    #Order Cancell Listener with integrated.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,11 +1,12 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
 use App\Events\Order\OrderPlaced;
+use App\Events\Order\OrderPaid;
+use App\Events\Order\OrderCancelled;
 
 use App\Listeners\OrderPlaced\ReserveInventory;
 use App\Listeners\OrderPlaced\GenerateOrderNo;
@@ -13,6 +14,18 @@ use App\Listeners\OrderPlaced\CreateTimeline;
 use App\Listeners\OrderPlaced\NotifyAdmin;
 use App\Listeners\OrderPlaced\SendEmail;
 
+use App\Listeners\OrderPaid\ReleaseInventory;
+use App\Listeners\OrderPaid\CreateInvoice;
+use App\Listeners\OrderPaid\CreateTimeline;
+use App\Listeners\OrderPaid\NotifyAdmin;
+use App\Listeners\OrderPaid\SendEmail;
+
+use App\Listeners\OrderCancelled\AnalyticActivity;
+use App\Listeners\OrderCancelled\CreateTimeline;
+use App\Listeners\OrderCancelled\LogOrderActivity;
+use App\Listeners\OrderCancelled\NotifyAdmin;
+use App\Listeners\OrderCancelled\RefundPayment;
+use App\Listeners\OrderCancelled\RestoreInventory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -40,12 +53,18 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(OrderPlaced::class, NotifyAdmin::class);
         Event::listen(OrderPlaced::class, SendEmail::class);  
 
-        Event::listen(\App\Events\Order\OrderPaid::class, \App\Listeners\OrderPaid\ReleaseInventory::class);
-        Event::listen(\App\Events\Order\OrderPaid::class, \App\Listeners\OrderPaid\CreateInvoice::class);
-        Event::listen(\App\Events\Order\OrderPaid::class, \App\Listeners\OrderPaid\CreateTimeline::class);
-        Event::listen(\App\Events\Order\OrderPaid::class, \App\Listeners\OrderPaid\NotifyAdmin::class);
-        Event::listen(\App\Events\Order\OrderPaid::class, \App\Listeners\OrderPaid\SendEmail::class); 
+        Event::listen(OrderPaid::class, ReleaseInventory::class);
+        Event::listen(OrderPaid::class, CreateInvoice::class);
+        Event::listen(OrderPaid::class, CreateTimeline::class);
+        Event::listen(OrderPaid::class, NotifyAdmin::class);
+        Event::listen(OrderPaid::class, SendEmail::class); 
             
+        Event::listen(OrderCancelled::class, AnalyticActivity::class);
+        Event::listen(OrderCancelled::class, CreateTimeline::class);
+        Event::listen(OrderCancelled::class, LogOrderActivity::class);
+        Event::listen(OrderCancelled::class, NotifyAdmin::class);
+        Event::listen(OrderCancelled::class, RefundPayment::class); 
+        Event::listen(OrderCancelled::class, RestoreInventory::class); 
 
     }
 }
